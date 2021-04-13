caf6f4f0b65cb41d88a5366e9998220f2396f530
mango <123456>
2021-04-13 09:53:53 +0800

    行动算子fold

--- spark-core/src/main/java/com/mango/bigdata/spark/core/rdd/operator/action/Spark03_RDD_Operator_Action.scala
+++ spark-core/src/main/java/com/mango/bigdata/spark/core/rdd/operator/action/Spark03_RDD_Operator_Action.scala
@@ -5,12 +5,12 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 /**
  * @author mango
- * @description 行动算子aggregate
+ * @description 行动算子fold
  * @email
  * @date 2021/4/13
  * @version 1.0
  */
-object Spark02_RDD_Operator_Action {
+object Spark03_RDD_Operator_Action {
   def main(args: Array[String]): Unit = {
 
     //TODO 准备环境
@@ -22,10 +22,10 @@ object Spark02_RDD_Operator_Action {
 
     /**
      * 预期14+16 = 30 结果40
-     * aggregateByKey: 初始值只会参与分区内计算
-     * aggregate： 初始值会参与分区内和分区间计算
+     * foldByKey: 初始值只会参与分区内计算
+     * fold： 初始值会参与分区内和分区间计算
      */
-    val i: Int = rdd.aggregate(10)(_+_,_+_)
+    val i: Int = rdd.fold(10)(_+_)
     println(i)
 
     //TODO 关闭环境
